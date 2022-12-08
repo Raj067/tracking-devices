e9c0a1a361a330bfc5aed527d0f92cfb48accfd8
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-12-09 00:21:16 +0300

    ---

--- lib/pages/devices/send_sms.dart
+++ lib/pages/devices/send_sms.dart
@@ -1,7 +1,19 @@
 import '../../backend/models/actions.dart';
+import 'package:telephony/telephony.dart';
 
 void sendSms(ActionsModel model) async {
-  if (model.phone != "") {
+  final Telephony telephony = Telephony.instance;
+  bool? permissionsGranted = await telephony.requestPhoneAndSmsPermissions;
+  if (model.phone != "" && permissionsGranted!) {
     print("helo world");
+    final SmsSendStatusListener listener = (SendStatus status) {
+      // Handle the status
+      print("The status is: ${status}");
+    };
+
+    telephony.sendSms(
+        to: model.phone,
+        message: "May the force be with you!",
+        statusListener: listener);
   }
 }
