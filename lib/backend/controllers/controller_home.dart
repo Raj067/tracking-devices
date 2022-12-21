1fbff92be65e37ce56d79b3221d0df47f428ed0b
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-12-21 09:06:46 +0300

    ---

--- lib/backend/controllers/controller_home.dart
+++ lib/backend/controllers/controller_home.dart
@@ -5,32 +5,4 @@ class HomeController extends GetxController {
   RxList<Devices> devices = <Devices>[].obs;
 
   var token = "".obs;
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  //   // devices = listDevices.obs;
-  // }
-
-  void search(String val) {
-    List filteredList = [];
-    List remainedList = [];
-    for (Devices dx in devices) {
-      if (stringLinearSearch(val: val, parent: "${dx.device}")) {
-        filteredList.add(dx);
-      } else {
-        remainedList.add(dx);
-      }
-    }
-    List<Devices> newList = [...filteredList, ...remainedList];
-    devices.value = newList;
-    update();
-  }
-
-  bool stringLinearSearch({
-    required String val,
-    required String parent,
-  }) =>
-      parent.toLowerCase().contains(
-            val.toLowerCase(),
-          );
 }
--- lib/pages/search_devices.dart
+++ lib/pages/search_devices.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../backend/controllers/controller_home.dart';
 import '../backend/models/actions.dart';
+import '../backend/models/devices.dart';
 import '../backend/models/types.dart';
-import 'devices/devices_list.dart';
+import 'api.dart';
+import 'device_details.dart';
 
-class SearchDevice extends StatelessWidget {
+class SearchDevice extends StatefulWidget {
   const SearchDevice({
     Key? key,
     required this.deviceType,
@@ -15,8 +19,40 @@ class SearchDevice extends StatelessWidget {
   final ActionsModel action;
 
   @override
+  State<SearchDevice> createState() => _SearchDeviceState();
+}
+
+class _SearchDeviceState extends State<SearchDevice> {
+  List<Devices> listDevices = [];
+  final controller = Get.put(HomeController());
+  bool isError = false;
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    try {
+      var response = await http.get(
+        Uri.parse("${baseUrl}get-devices/${widget.deviceType.id}/"),
+        headers: getAuthHeaders(controller.token.value),
+      );
+      List<Devices> allTypes = [];
+      // print(jsonDecode(response.body));
+      for (var dx in jsonDecode(response.body)) {
+        allTypes.add(fromJsonDevices(dx));
+      }
+      listDevices = allTypes;
+    } catch (e) {
+      isError = true;
+    }
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final controller = Get.put(HomeController());
     return Scaffold(
       appBar: AppBar(
         title: TextFormField(
@@ -28,14 +64,76 @@ class SearchDevice extends StatelessWidget {
               hintText: 'Search Device..',
             ),
             onChanged: (val) {
-              controller.search(val);
+              search(val);
             }),
         elevation: 0,
       ),
-      body: DevicesList(
-        deviceType: deviceType,
-        action: action,
-      ),
+      body: listDevices.isEmpty && !isError
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : isError
+              ? Center(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isError = false;
+                      });
+                      fetchData();
+                    },
+                    child: const Text("Network error: Reload"),
+                  ),
+                )
+              : Scrollbar(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: () {
+                          // Get.to(SingleDevice(device: listDevices[index]));
+                          Get.to(
+                            DevicesDetails(
+                              device: listDevices[index],
+                              deviceType: widget.deviceType,
+                              action: widget.action,
+                            ),
+                          );
+                        },
+                        title: Text(
+                          listDevices[index].device,
+                        ),
+                        subtitle: Text(
+                          widget.deviceType.name,
+                        ),
+                      );
+                    },
+                    itemCount: listDevices.length,
+                  ),
+                ),
     );
   }
+
+  void search(String val) {
+    List filteredList = [];
+    List remainedList = [];
+    for (Devices dx in listDevices) {
+      if (stringLinearSearch(val: val, parent: dx.device)) {
+        filteredList.add(dx);
+      } else {
+        remainedList.add(dx);
+      }
+    }
+    List<Devices> newList = [...filteredList, ...remainedList];
+
+    setState(() {
+      listDevices = newList;
+    });
+  }
+
+  bool stringLinearSearch({
+    required String val,
+    required String parent,
+  }) =>
+      parent.toLowerCase().contains(
+            val.toLowerCase(),
+          );
 }
